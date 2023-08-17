837be492b55949204771bb2440d0f36b700003a9
MiyuFujitsuka <shiraduki.94@gmail.com>
2023-08-17 14:37:26 +0000

    Initial Commit

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,8 @@
+class PostCommentsController < ApplicationController
+  
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
+  end
+  
+end
